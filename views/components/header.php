5dd521cc974e60e0128e7be161fb9d542ca14200
asciito <ayax.cordova@aydev.mx>
2024-01-30 19:21:00 -0600

    style: fix template structure

--- views/components/header.php
+++ views/components/header.php
@@ -107,3 +107,7 @@
             <h1 class="text-3xl font-bold tracking-tight text-gray-900"><?= $title ?> Page</h1>
         </div>
     </header>
+
+    <!-- MAIN -->
+    <main class="min-h-full">
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
--- views/dashboard.php
+++ views/dashboard.php
@@ -1,10 +1,3 @@
 <?php view('components.header', ['title' => $title]); ?>
-
-<!-- MAIN -->
-<main class="min-h-full">
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <!-- Your content -->
-    </div>
-</main>
-
+    <!-- Your content -->
 <?php view('components.footer') ?>
